5e5f5ed95bde50ed205e02a83ac71b18a2d7c041
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-11-15 12:35:59 -0600

    removed bad formats

--- AdrianaJauregui, ElizabethRamirez/ToDoApp/ToDoApp/StaticToDoViewerController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StaticToDoViewerController.swift
-//  ToDoApp
-//
-//  Created by Adriana Jauregui on 09/11/19.
-//  Copyright © 2019 Adriana Jauregui. All rights reserved.
-//
-
-import Foundation
